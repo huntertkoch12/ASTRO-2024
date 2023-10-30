00bf5bbe37756a8638c7573a242e46ea58a18499
RND <123332740+RP-ND@users.noreply.github.com>
2023-10-30 18:47:14 -0400

    Update astro.ino

--- ASTRO/astro.ino
+++ ASTRO/astro.ino
@@ -1,1 +1,27 @@
+#include "config.h"
+
+extern void initRadio();
+extern void receiveAndReply();
+
+void setup() {
+  // Initialization for core 0 (if any)
+  Serial.begin(115200);
+  
+  while (!Serial) delay(1);
+
+  Serial.println("Initialization on Core 0!");
+}
+
+void loop() {
+  // Main loop for core 0 (if any)
+  // This can be left empty if not needed
+}
+
+void setup1() {
+  initRadio();
+}
+
+void loop1() {
+  receiveAndReply();
+}
 
