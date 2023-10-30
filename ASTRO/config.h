cf11177ed1fc9a338a9598c4112e8feae505b124
RND <123332740+RP-ND@users.noreply.github.com>
2023-10-30 19:19:50 -0400

    Update config.h

--- ASTRO/config.h
+++ ASTRO/config.h
@@ -1,32 +1,58 @@
 #pragma once
 
-// Including required libraries
+//*******************************************//
+//             Include Libraries             //
+//*******************************************//
+
 #include <Arduino.h>
 #include <RH_RF95.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include <SdFat.h>
+#include <SPI.h>
 
-// Defining settings for LoRa communication
-#define RF95_FREQ 915.0            // LoRa Frequency in MHz
-#define RFM95_CS 16                // Chip Select Pin for LoRa
-#define RFM95_INT 21               // Interrupt Pin for LoRa
-#define RFM95_RST 17               // Reset Pin for LoRa
-#define TX_POWER 23                // Transmit Power for LoRa in dBm
-#define BANDWIDTH 125              // Bandwidth for LoRa in kHz
-#define SPREADING_FACTOR 7         // Spreading Factor for LoRa
-#define CODING_RATE 5              // Coding Rate for LoRa (4/x)
-#define PREAMBLE_LENGTH 8          // Preamble Length for LoRa
+//*******************************************//
+//             LoRa Settings                 //
+//*******************************************//
 
-// Defining settings for BNO055 I2C communication
-#define BNO055_SDA_PIN 4           // SDA pin for BNO055
-#define BNO055_SCL_PIN 5           // SCL pin for BNO055
+#define RF95_FREQ 915.0            // LoRa Frequency (MHz)
+#define RFM95_CS 16                // Chip Select Pin (LoRa)
+#define RFM95_INT 21               // Interrupt Pin (LoRa)
+#define RFM95_RST 17               // Reset Pin (LoRa)
+#define TX_POWER 23                // Transmit Power (LoRa, dBm)
+#define BANDWIDTH 125              // Bandwidth (LoRa, kHz)
+#define SPREADING_FACTOR 7         // Spreading Factor (LoRa)
+#define CODING_RATE 5              // Coding Rate (LoRa, 4/x)
+#define PREAMBLE_LENGTH 8          // Preamble Length (LoRa)
 
-// Externally declaring the LoRa radio object for use in other files
-extern RH_RF95 rf95;
+//*******************************************//
+//             BNO055 Settings               //
+//*******************************************//
 
-// Function prototypes for initializing and operating the radio and BNO055 sensor
-void initRadio();
-void receiveAndReply();
-void initBNO055();
-void logBNO055Data();
+#define BNO055_SDA_PIN 4           // SDA pin (BNO055)
+#define BNO055_SCL_PIN 5           // SCL pin (BNO055)
+
+//*******************************************//
+//           External Declarations           //
+//*******************************************//
+
+extern RH_RF95 rf95;              // LoRa Radio Object
+extern SdFat sd;                  // SD Card Object
+extern SdFile dataFile;           // Data File Object
+
+//*******************************************//
+//         Function Prototypes               //
+//*******************************************//
+
+// LoRa Radio Functions
+void initRadio();                 // Initialize LoRa Radio
+void receiveAndReply();           // Receive Message & Send Reply
+
+// BNO055 Sensor Functions
+void initBNO055();                // Initialize BNO055 Sensor
+void logBNO055Data();             // Log Data from BNO055 Sensor
+
+// SD Card Functions
+void setupSDCard();               // Initialize SD Card
+void logDataToSD();               // Log Data to SD Card
